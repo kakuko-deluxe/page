45f6c054a479e5967bbe2638085854d01b58ee44
kakuko-deluxe <ky77ry@gmail.com>
2019-07-14 01:22:16 +0900

    ActiveRecord::StatementInvalid in Posts#index マイグレーションエラー DBにデータがない？

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [ :show, :edit, :update, :destroy]
   def index
-    @posts = Post.all
-    @new_posts = Post.all
+    @posts = Post.order(create_at: :desc)
+    @new_posts = Post.order(create_at: :desc).limit(5)
   end
   def new
     @post = Post.new #フォーム用の空インスタンス
@@ -17,13 +18,13 @@ class PostsController < ApplicationController
     end
   end
   def show
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
   end
   def edit
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
   end
   def update
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to @post, notice: "ブログを更新しました。"
     else
@@ -31,7 +32,7 @@ class PostsController < ApplicationController
     end
   end
 def destroy
-  @post = Post.find(params[:id])
+  # @post = Post.find(params[:id])
   @post.destroy
   redirect_to posts_path, notice: "ブログを削除しました"
 end
@@ -41,5 +42,8 @@ end
 
   def post_params #ストロングパラメータ定義
     params.require(:post).permit(:title, :body)
+  end
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
