e6d94867f572127abc4381770ab93b8b434c45f4
kakuko-deluxe <ky77ry@gmail.com>
2019-07-14 01:09:00 +0900

    My_blog1終了 リファクタリング前

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,45 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    @new_posts = Post.all
+  end
+  def new
+    @post = Post.new #フォーム用の空インスタンス
+  end
+  def create
+    @post = Post.new(post_params) #ストロングパラメーターを引数に
+    # @post.save
+    # redirect_to @post, notice:"ブログを登録しました。" #showページにリダイレクト
+    if @post.save
+      redirect_to @post, notice: "ブログを登録しました。"
+    else
+      render :new
+    end
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: "ブログを更新しました。"
+    else
+      render :edit
+    end
+  end
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect_to posts_path, notice: "ブログを削除しました"
+end
+
+
+  private 
+
+  def post_params #ストロングパラメータ定義
+    params.require(:post).permit(:title, :body)
+  end
+end
